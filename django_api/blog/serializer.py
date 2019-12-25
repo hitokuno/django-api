ba76799376f34c087cb6dec4fa532bb663f226cd
hitokuno <hitokuno@aol.com>
2019-12-25 15:31:58 +0900

    Enalbe to post with Author id

--- django_api/blog/serializer.py
+++ django_api/blog/serializer.py
@@ -9,9 +9,18 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
+        extra_kwargs = { 'id': { 'read_only': False } }
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer()
     class Meta:
         model = Post
         fields = ('id', 'author', 'title', 'text', 'created_date', 'published_date', 'attach')
+        read_only_fields = ('created_at', 'updated_at')
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('author')
+        user = User.objects.get(pk=user_data['id'])
+        post = Post.objects.create(author=user, **validated_data)
+        return post
+
